3a92dbb4bf880a90a7c9ee20199035ec2b6ae687
estefaniacn <85124911+estefaniacn@users.noreply.github.com>
2023-03-16 11:56:59 -0400

    feat: add new project random quotes

---
+++ citas-aleatorias/script.js
@@ -0,0 +1,37 @@
+let citas = [
+  {
+    "autor": "Albert Einstein",
+    "texto": "No poseo talentos especiales. Solo soy apasionadamente curioso."
+  },
+  {
+    "autor": "Anónimo",
+    "texto": "Semanas de programación te pueden ahorrar horas de planeación."
+  }
+];
+
+// Para generar indices aleatorios.
+function generarEnteroAleatorio(minimo, maximo) {
+  minimo = Math.ceil(minimo);
+  maximo = Math.floor(maximo);
+  // Incluye el minimo pero no el maximo.
+  return Math.floor(Math.random() * (maximo - minimo) + minimo);
+}
+
+function cambiarCita() {
+  let indiceAleatorio = generarEnteroAleatorio(0, citas.length);
+  citaElem.textContent = `"${citas[indiceAleatorio].texto}"`;
+  autorElem.textContent = citas[indiceAleatorio].autor;
+}
+
+// Obtener los elementos HTML.
+let botonElem = document.getElementById('boton-cambiar-cita');
+let citaElem = document.getElementById('cita');
+let autorElem = document.getElementById('autor');
+
+// Para seleccionar una cita de forma aleatoria
+// cuando se inicia la aplicacion.
+let indiceAleatorio = generarEnteroAleatorio(0, citas.length);
+cambiarCita();
+
+// Para cambiar la cita al hacer click en el boton.
+botonElem.addEventListener('click', cambiarCita);
