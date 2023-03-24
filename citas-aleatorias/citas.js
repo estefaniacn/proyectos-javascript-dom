7b69e38e7f40b10eb201ece45e6306babda32b3d
estefaniacn <85124911+estefaniacn@users.noreply.github.com>
2023-03-24 16:50:41 -0400

    feat: add new quotes to the random quotes project

--- citas-aleatorias/citas.js
+++ citas-aleatorias/citas.js
@@ -1,10 +1,42 @@
 let citas = [
   {
-    "autor": "Albert Einstein",
-    "texto": "No poseo talentos especiales. Solo soy apasionadamente curioso."
+    'autor': 'Albert Einstein',
+    'texto': 'No poseo talentos especiales. Solo soy apasionadamente curioso.'
   },
   {
-    "autor": "Anónimo",
-    "texto": "Semanas de programación te pueden ahorrar horas de planeación."
+    'autor': 'Anónimo',
+    'texto': 'Semanas de programación te pueden ahorrar horas de planeación.'
+  },
+  {
+    'autor': 'Alan Kay',
+    'texto': 'La mejor forma de predecir el futuro es inventarlo.'
+  },
+  {
+    'autor': 'Amelia Earhart',
+    'texto': 'Lo más dificil es tomar la decisión de actuar. El resto es simplemente tenacidad.'
+  },
+  {
+    'autor': 'Aristotle',
+    'texto': 'La calidad no es un acto, es un hábito.'
+  },
+  {
+    'autor': 'Benjamin Franklin',
+    'texto': 'Dímelo y lo olvidaré. Enséñamelo y lo recordaré. Involúcrame y lo aprenderé.'
+  },
+  {
+    'autor': 'Charles R. Swindoll',
+    'texto': 'La vida es 10% lo que te ocurre y 90% cómo reaccionas.'
+  },
+  {
+    'autor': 'Jane Goodall',
+    'texto': 'Lo que haces hace una diferencia. Y debes decidir si qué tipo de diferencia quieres hacer.'
+  },
+  {
+    'autor': 'John Muir',
+    'texto': 'El poder de la imaginación nos hace infinitos'
+  },
+  {
+    'autor': 'Mark Twain',
+    'texto': 'Los dos días más importantes de tu vida son el día que naciste y el día que descubres por qué.'
   }
 ];
