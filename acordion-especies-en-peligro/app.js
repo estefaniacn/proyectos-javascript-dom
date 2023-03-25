d8e2809df30ca79bba7a68aa7d93fa2f03dc00cc
estefaniacn <85124911+estefaniacn@users.noreply.github.com>
2023-03-25 11:38:43 -0400

    refactor: update accordion project
    Update the style, add Google Fonts, and improve
    the structure of the CSS file. Add new comments
    to the JavaScript file.

--- acordion-especies-en-peligro/app.js
+++ acordion-especies-en-peligro/app.js
@@ -1,9 +1,10 @@
+// Seleccionar lista de contenedores.
 const contenedores = document.getElementsByClassName('contenedor');
 
-console.log(contenedores);
-
+// Asignar un event listener a cada contenedor de la lista
+// de especies en peligro de extincion.
 for (i = 0; i < contenedores.length; i++) {
   contenedores[i].addEventListener('click', function () {
     this.classList.toggle('activo');
-  })
+  });
 };
