ca8306633da63cfdbb45812f9f80ab4a7fbf783e
estefaniacn <85124911+estefaniacn@users.noreply.github.com>
2023-03-25 16:05:36 -0400

    refactor: update periodic table project
    Create a new file with the data of the elements.
    Update comments and structure of the files.

--- tabla-periodica/script.js
+++ tabla-periodica/script.js
@@ -1,102 +1,11 @@
+// Seleccionar elementos.
 const listaDeElementos = document.getElementById('lista-elementos');
 const infoElemento = document.getElementById('info-elemento');
 const numeroAtomicoElem = document.getElementById('numero-atomico');
 const nombreElemento = document.getElementById('nombre');
 const simboloElem = document.getElementById('simbolo');
 
-let datos = {
-  'Hidrógeno': {
-    'simbolo': 'H',
-    'numeroAtomico': 1,
-    'grupo': 'no metal'
-  },
-  'Helio': {
-    'simbolo': 'He',
-    'numeroAtomico': 2,
-    'grupo': 'gas noble'
-  },
-  'Litio': {
-    'simbolo': 'Li',
-    'numeroAtomico': 3,
-    'grupo': 'metal alcalino'
-  },
-  'Berilio': {
-    'simbolo': 'Be',
-    'numeroAtomico': 4,
-    'grupo': 'metal alcalinoterreo'
-  },
-  'Boro': {
-    'simbolo': 'B',
-    'numeroAtomico': 5,
-    'grupo': 'no metal'
-  },
-  'Carbono': {
-    'simbolo': 'C',
-    'numeroAtomico': 6,
-    'grupo': 'no metal'
-  },
-  'Nitrógeno': {
-    'simbolo': 'N',
-    'numeroAtomico': 7,
-    'grupo': 'no metal'
-  },
-  'Oxígeno': {
-    'simbolo': 'O',
-    'numeroAtomico': 8,
-    'grupo': 'metaloide'
-  },
-  'Flúor': {
-    'simbolo': 'F',
-    'numeroAtomico': 9,
-    'grupo': 'halogeno'
-  },
-  'Neón': {
-    'simbolo': 'Ne',
-    'numeroAtomico': 10,
-    'grupo': 'gas noble'
-  },
-  'Sodio': {
-    'simbolo': 'Na',
-    'numeroAtomico': 11,
-    'grupo': 'metal alcalino'
-  },
-  'Magnesio': {
-    'simbolo': 'Mg',
-    'numeroAtomico': 12,
-    'grupo': 'metal alcalinoterreo'
-  },
-  'Aluminio': {
-    'simbolo': 'Al',
-    'numeroAtomico': 13,
-    'grupo': 'metal'
-  },
-  'Silicio': {
-    'simbolo': 'Si',
-    'numeroAtomico': 14,
-    'grupo': 'metaloide'
-  },
-  'Fósforo': {
-    'simbolo': 'P',
-    'numeroAtomico': 15,
-    'grupo': 'no metal'
-  },
-  'Azufre': {
-    'simbolo': 'S',
-    'numeroAtomico': 16,
-    'grupo': 'no metal'
-  },
-  'Cloro': {
-    'simbolo': 'Cl',
-    'numeroAtomico': 17,
-    'grupo': 'halogeno'
-  },
-  'Argón': {
-    'simbolo': 'Ar',
-    'numeroAtomico': 18,
-    'grupo': 'gas noble'
-  }
-};
-
+// Colores para cada grupo de elementos.
 let colores = {
   'metaloide': '#12c540',
   'no metal': '#349cc7',
@@ -107,6 +16,8 @@ let colores = {
   'halogeno': '#e62dbd'
 };
 
+// Agregar elementos a partir de los datos 
+// automaticamente.
 function agregarElementos(datos) {
   for (let i = 0; i < Object.keys(datos).length; i++) {
     const elementoNuevo = document.createElement("li");
@@ -122,6 +33,7 @@ agregarElementos(datos);
 
 let elementos = document.getElementsByClassName('elemento');
 
+// Mostrar un elemento al momento de abrir la pagina web.
 function mostrarElementoInicial() {
   const nombre = elementos[0].textContent;
   numeroAtomicoElem.textContent = datos[nombre].numeroAtomico;
